708925767378ee9d4af12c540f82103183aca296
Sambit Pradhan <sambitgulu@gmail.com>
2024-05-18 11:34:57 +0530

    commit

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Add comment for below command during code submission on Geektrust Platform
-#./gradlew clean build -x test --no-daemon
+./gradlew clean build -x test --no-daemon
 
 # Remove the below comment during code submission on Geektrust Platform
-gradle clean build -x test --no-daemon
+#gradle clean build -x test --no-daemon
 
 java -jar build/libs/geektrust.jar sample_input/input1.txt
